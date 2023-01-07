0af1bff0959ef8b006a0206f1c444538aa4943ce
boskuv <boris_kv@bk.ru>
2023-01-07 16:40:12 +0300

    temp: add some code for logger

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import time
+from pathlib import Path
 
 from prometheus_client import start_http_server
 
 from app.config import load_config
+from app.utils.logger import customize_logging
 from app.handlers import check_file_changes, analyze_audit_file
 
 
 """
 TODO
 - audit file rotation
+- memorize the line offset in db
 - compare hash tmp and log
 - yaml config
-- test counter auto renew in prometheus
 - /home/user/Projects/vault/Vault-Audit-Prometheus-Exporter/app/handlers/analyze_audit_file.py:32: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
 
-from app.db.db import *
+
 import pyhash
 hasher = pyhash.metro_64()
 hasher("")
-from prometheus_client import Counter
 """
 
 
 if __name__ == "__main__":
-    config = load_config("exporter.ini")  # TODO: 1) check if exists 2) sys.argv[1] or default
+    config = load_config(
+        "exporter.init"
+    )  # TODO: 1) check if exists 2) sys.argv[1] or default
+
+    # logger = customize_logging(
+    #     filepath=Path(config.logging.path),
+    #     level=config.logging.level,
+    #     retention=config.logging.retention,
+    #     rotation=config.logging.rotation,
+    #     _format=config.logging.format,
+    # )
 
     start_http_server(config.prometheus.port)
 
